b51898d4ecc3060bbee39a9c1f655376b7121d84
Dongyu Li <dongyul@andrew.cmu.edu>
2017-07-20 11:08:52 -0400

    mysql exploit behav and data

---
+++ vulnerabilities/mysql(race)/exploit-mysql/provision/install_mysql.sh
@@ -0,0 +1,17 @@
+# uninstall mariadb
+sudo dnf remove -y mariadb
+
+cd /home/vagrant
+wget https://downloads.mysql.com/archives/get/file/MySQL-5.5.51-1.el7.x86_64.rpm-bundle.tar 
+tar xvf MySQL-*.rpm-bundle.tar
+
+# resolve dependency issues 
+#sudo dnf install -y libnuma.so.1 mecab mecab-ipadic perl-Memoize perl-Time-Hires
+sudo dnf install -y libncurses.so.5 libtinfo.so.5 
+
+# install mysql
+sudo rpm -ivh MySQL-*.rpm
+
+#starting mysql 
+sudo service mysql start 
+
