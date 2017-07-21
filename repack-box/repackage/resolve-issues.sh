29109e6aed86903a30989865fb874de8806b4863
Dongyu Li <dongyul@andrew.cmu.edu>
2017-07-21 10:13:03 -0400

    cleanup script for repack box

--- repack-box/repackage/resolve-issues.sh
+++ repack-box/repackage/resolve-issues.sh
@@ -19,18 +19,12 @@ sudo grub2-set-default "Fedora (4.11.6camflow-0.3.3) 25 (Workstation Edition)"
 
 
 # solve SSH issues that would have occurred after packaging the box
-echo 0
 sudo rm /home/vagrant/.ssh/*
-echo 1
 
 sudo wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys 
-echo 2 
 sudo chmod 0700 /home/vagrant/.ssh  
-echo 3
 sudo chmod 0600 /home/vagrant/.ssh/authorized_keys 
-echo 4
 sudo chown -R vagrant /home/vagrant/.ssh
-echo 5
 
 #/vagrant/provision/expect.sh
 
