b571c9ecc0b39e6974157be891ae781bb45ede9b
Dongyu Li <dongyul@andrew.cmu.edu>
2017-08-01 13:30:58 -0400

    parameterized input and output file paths with options.

--- graph-visualization/visualizer/visualizer.c
+++ graph-visualization/visualizer/visualizer.c
@@ -5,6 +5,20 @@
 //  Created by Michael Hahn on 6/5/17.
 //  Copyright © 2017 Michael Hahn. All rights reserved.
 //
+//  Enhanced by Dennis Li 08/01/2017
+//  1. Store pixel matrix on heap instead of stack: from processing about 800 nodes to about 9.8k nodes 
+//      for under a miniute, which is the largest provenance dataset we have at this time. (Created 
+//      the alloc_matrix and free_matrix functions.)
+//  2. Added a preprossessing stage of the input file to automatically determine the width and height of
+//      the matrix being generated. (Efficency can possibly be impproved with given features of the input
+//      file, such as the region where the max numbered node should occer in the file, i.e. towards the 
+//      end.)
+//  3. Parameterized the input file path and the output file path of this program, identified by -i and -o, 
+//     respectively, on the command line. (Added the get_files function.)
+//  
+//  Usage of this program: 
+//  ./program_name -i input_file_path.txt -o output_file_path.bmp
+//
 
 #include <stdio.h>
 #include <string.h>
@@ -12,11 +26,14 @@
 #include <inttypes.h>
 #include <math.h>
 #include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
 
-// #define WIDTH 7840//in pixels
-// #define HEIGHT 7840 //in pixels
-#define FILENAME "/Users/DongyuLi/Desktop/Interns17/graph-visualization/output/graph1_auto_width.bmp"
-#define INPUTFILE "/Users/DongyuLi/Desktop/Interns17/graph-visualization/edgelist/edgelist1.txt"
+#define ARGLEN 100
+// #define width 7840//in pixels
+// #define height 7840 //in pixels
+// #define FILENAME "/Users/DongyuLi/Desktop/Interns17/graph-visualization/output/graph1_auto_width.bmp"
+// #define INPUTFILE "/Users/DongyuLi/Desktop/Interns17/graph-visualization/edgelist/edgelist1.txt"
 
 unsigned int hash(char *str, int length)
 {
@@ -40,6 +57,9 @@ uint64_t convert(const char *text)
    return number;
 }
 
+
+// allocate heap memory for a 2-D matrix with height height, width width, 
+// and each element has size elem_size. 
 void** alloc_matrix(size_t height, size_t width, size_t elem_size){
     void** result = malloc(height * sizeof(void*));
     if(result == NULL){
@@ -59,6 +79,8 @@ void** alloc_matrix(size_t height, size_t width, size_t elem_size){
     return result;
 }
 
+
+// free heap memory for a 2-D matrix matrix with height height.
 void free_matrix(void** matrix, size_t height){
     for(size_t i = 0; i < height; i++){
         free(matrix[i]);
@@ -66,11 +88,44 @@ void free_matrix(void** matrix, size_t height){
     free(matrix);
 }
 
+// getting input file path and output file path from command line by options -i for input, and -o for 
+// output
+void get_files(char** inputfile, char** outputfile, int argc, char* argv[], size_t filename_length){
+    int index;
+    int c;
+    while((c = getopt(argc, argv, "i:o:")) != -1){
+        switch(c){
+            case 'i': 
+                *inputfile = optarg;
+                break; 
+            case 'o':
+                *outputfile = optarg;
+                break;
+            case '?': 
+                if(optopt == 'i' | optopt == 'o')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+                exit(-1);
+            default:
+                abort();
+        }
+    }
+
+    for (index = optind; index < argc; index++){
+        printf ("Non-option argument %s\n", argv[index]);
+        exit(-1);
+    }
+
+}
 
 int main(int argc, const char * argv[]) {
+    char* inputfile;
     FILE * input;
-    size_t WIDTH = 0;
-    size_t HEIGHT = 0;
+    size_t width = 0;
+    size_t height = 0;
     size_t max_node = 0;
     size_t from, to;
     char line[100];
@@ -79,6 +134,7 @@ int main(int argc, const char * argv[]) {
     char * colortoken;
     uint64_t fromlabel, tolabel, edgelabel;
 
+    char* outputfile;
     FILE * output;
     unsigned int bitmapfileheader[3]; //combined bfReserved1 and bfReserved2
     unsigned int bitmapinfoheader[9]; //no biPlanes or biBitCount as they are shorts not ints
@@ -89,9 +145,12 @@ int main(int argc, const char * argv[]) {
     int red, green, blue;
 
 
+    //getting input and output file names from command line
+    get_files(&inputfile, &outputfile, argc, (char**) argv, ARGLEN);
+
     // * produce matrix of pixel data from edgelist
     //open the file to read edge list
-    input = fopen(INPUTFILE, "r");
+    input = fopen(inputfile, "r");
     if (input == NULL) {
         perror("Error opening input file\n");
         return -1;
@@ -109,16 +168,16 @@ int main(int argc, const char * argv[]) {
             max_node = to;
         }
     }
-    WIDTH = max_node + 1; 
-    HEIGHT = WIDTH;
-    printf("width == height == %d\n", (int)WIDTH);
+    width = max_node + 1; 
+    height = width;
+    // printf("width == height == %d\n", (int)width);
     
-    // uint64_t frompixels[WIDTH][HEIGHT];
-    // uint64_t topixels[WIDTH][HEIGHT];   
-    //uint64_t edgepixels[WIDTH][HEIGHT];
-    uint64_t** frompixels = (uint64_t**)alloc_matrix(HEIGHT, WIDTH, sizeof(uint64_t));
-    uint64_t** topixels = (uint64_t**)alloc_matrix(HEIGHT, WIDTH, sizeof(uint64_t));
-    uint64_t** edgepixels = (uint64_t**)alloc_matrix(HEIGHT, WIDTH, sizeof(uint64_t));
+    // uint64_t frompixels[width][height];
+    // uint64_t topixels[width][height];   
+    // uint64_t edgepixels[width][height];
+    uint64_t** frompixels = (uint64_t**)alloc_matrix(height, width, sizeof(uint64_t));
+    uint64_t** topixels = (uint64_t**)alloc_matrix(height, width, sizeof(uint64_t));
+    uint64_t** edgepixels = (uint64_t**)alloc_matrix(height, width, sizeof(uint64_t));
     if(!frompixels | !topixels | !edgepixels){
         perror("Error opening ouput file\n"); 
         return -1;
@@ -127,8 +186,8 @@ int main(int argc, const char * argv[]) {
 
 
     //initialize all pixels to 0
-    for (x = 0; x < WIDTH; x++) {
-        for (y = 0; y < HEIGHT; y++) {
+    for (x = 0; x < width; x++) {
+        for (y = 0; y < height; y++) {
             frompixels[x][y] = 0;
             topixels[x][y] = 0;
             edgepixels[x][y] = 0;
@@ -164,14 +223,14 @@ int main(int argc, const char * argv[]) {
     fclose(input);
     
 
-    printf("here 1\n");
+
     // * produce dmp file
     //determine the extra padding size per line
-    extrapaddingperline = 4 - ((WIDTH * 3) % 4);
+    extrapaddingperline = 4 - ((width * 3) % 4);
     if (extrapaddingperline == 4) extrapaddingperline = 0;
     
     //determine total size of the graph, excluding header size
-    totalsize = ((WIDTH * 3) + extrapaddingperline) * HEIGHT;
+    totalsize = ((width * 3) + extrapaddingperline) * height;
     
     //bitmap file header
     //First two bytes are `B` and `M`
@@ -181,8 +240,8 @@ int main(int argc, const char * argv[]) {
     
     //bitmap info header
     bitmapinfoheader[0] = 40; //biSize
-    bitmapinfoheader[1] = WIDTH; //biWidth
-    bitmapinfoheader[2] = WIDTH; //biHeight
+    bitmapinfoheader[1] = width; //biwidth
+    bitmapinfoheader[2] = width; //biheight
     //Skip biPlanes and biBitCount since they are shorts
     bitmapinfoheader[3] = 0; //biCompression
     bitmapinfoheader[4] = totalsize; //biSizeImage; it is okay to set it to 0 if no compression
@@ -192,19 +251,15 @@ int main(int argc, const char * argv[]) {
     bitmapinfoheader[8] = 0; //biClrImportant
     
     //open the file and start writing to the file
-    output = fopen(FILENAME, "wb");
+    output = fopen(outputfile, "wb");
     if (output == NULL) {
         perror("Error opening ouput file\n"); 
         return -1;
     }
-    
 
-    printf("here 2\n");
 
     //write bitmap file header to the file
-    // printf("here 2.1\n");
     fprintf(output, "BM");
-    // printf("here 2.2\n");
     for (n = 0; n < 3; n++) {
         // printf("n=%d\n", n);
         fprintf(output, "%c", bitmapfileheader[n] & 0x000000FF);
@@ -213,7 +268,7 @@ int main(int argc, const char * argv[]) {
         fprintf(output, "%c", (bitmapfileheader[n] & (unsigned int) 0xFF000000) >> 24);
     }
     
-    printf("here 3\n");
+
     //write bitmap info header to the file
     for (n = 0; n < 3; n++) {
         fprintf(output, "%c", bitmapinfoheader[n] & 0x000000FF);
@@ -222,14 +277,14 @@ int main(int argc, const char * argv[]) {
         fprintf(output, "%c", (bitmapinfoheader[n] & (unsigned int) 0xFF000000) >> 24);
     }
 
-    printf("here 4\n");
+
     //biPlanes and biBitCount fields.
     fprintf(output, "%c", 1); //must be set to 1
     fprintf(output, "%c", 0);
     fprintf(output, "%c", 24); //24 bits per pixel, a total of 16.7 million colors
     fprintf(output, "%c", 0);
     
-    printf("here 5\n");
+
     for (n = 3; n < 9; n++) {
         fprintf(output, "%c", bitmapinfoheader[n] & 0x000000FF);
         fprintf(output, "%c", (bitmapinfoheader[n] & 0x0000FF00) >> 8);
@@ -238,8 +293,8 @@ int main(int argc, const char * argv[]) {
     }
 
     //headers are done. Write data now
-    for (y = HEIGHT - 1; y >= 0; y--) { //BMP images are proceeded from the bottom to the top
-        for (x = 0; x < WIDTH; x++) {
+    for (y = height - 1; y >= 0; y--) { //BMP images are proceeded from the bottom to the top
+        for (x = 0; x < width; x++) {
             //fprintf(stdout, "%llu\n", frompixels[x][y]);
             if(frompixels[x][y] == 0 && topixels[x][y] == 0 && edgepixels[x][y] == 0){
                 red = 0; 
@@ -285,9 +340,9 @@ int main(int argc, const char * argv[]) {
     
     //close the file when done
     fclose(output);
-    free_matrix((void**)frompixels, HEIGHT);
-    free_matrix((void**)topixels, HEIGHT);
-    free_matrix((void**)edgepixels, HEIGHT);
+    free_matrix((void**)frompixels, height);
+    free_matrix((void**)topixels, height);
+    free_matrix((void**)edgepixels, height);
     
     return 0;
 }
