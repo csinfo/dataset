c85d7e18003b5cffc637c3af163b18b161858704
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-08-07 16:23:17 -0400

    add comments

--- vulnerabilities/mariadb(race)/normal-mariadb/provision/install-mysql-python-api.sh
+++ vulnerabilities/mariadb(race)/normal-mariadb/provision/install-mysql-python-api.sh
@@ -1,5 +1,5 @@
 
 cd /home/vagrant
-
+# install mysql package in python
 sudo wget "ftp://rpmfind.net/linux/fedora/linux/releases/23/Everything/x86_64/os/Packages/p/python-mysql-1.3.6-4.fc23.x86_64.rpm"
 sudo dnf install -y python-mysql-1.3.6-4.fc23.x86_64.rpm
