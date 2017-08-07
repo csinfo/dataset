9bf8f8656251d0aea12569568c5d10c6afd188e1
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-08-07 16:14:38 -0400

    add comments

--- vulnerabilities/mariadb(race)/normal-mariadb/provision/install_mariadb.sh
+++ vulnerabilities/mariadb(race)/normal-mariadb/provision/install_mariadb.sh
@@ -1,17 +1,20 @@
-# uninstall mariadb, from LAMP
+# uninstall mariadb in case it has the latest version since we are exploiting an older version
 sudo dnf remove -y mariadb
 
-# download mariadb 
+# download mariaDB
 cd /home/vagrant
 wget -r --no-parent -nH -nd -np -R index.html* http://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-10.1.17/yum/fedora24-amd64/rpms/ 
 
-# resolve dependency issues
+# resolve dependency issues before install older version of marinaDB
 sudo dnf install -y libthrift-0.9.1.so libodbc.so.2 libJudy.so.1 jemalloc perl-Time-HiRes perl-DBI perl-Memoize galera
 
+# install the older version of MarinaDB 10.1.17
 sudo rpm -ivh MariaDB-*
-# start 
+# start marinaDB
 sudo systemctl start mysql
 
 # setting mariadb and root passwd
-printf '\ny\nvagrant\nvagrant\ny\ny\ny\ny' | mysql_secure_installation 
+printf '\ny\nvagrant\nvagrant\ny\ny\ny\ny' | mysql_secure_installation
+
+# now after the last command, marinaDB will be running
 
