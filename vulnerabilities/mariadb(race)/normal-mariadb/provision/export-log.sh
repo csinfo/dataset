ccd9b6b06add33e4f62d72e857f11e8885bb3c3c
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-08-07 16:42:04 -0400

    add stop cam flow

--- vulnerabilities/mariadb(race)/normal-mariadb/provision/export-log.sh
+++ vulnerabilities/mariadb(race)/normal-mariadb/provision/export-log.sh
@@ -1,1 +1,7 @@
-cp /tmp/audit.log /vagrant/prov-data/audit.log
+# stop camflow capturing 
+sleep 5
+sudo camflow -e false
+# wait a while so that relay fs writes all the data to audit.log
+sleep 30
+# copy the audit log to another folder
+cp /tmp/audit.log /vagrant/prov-data/mariadb-race-normal.log
