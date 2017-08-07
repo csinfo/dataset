57cc58226670fcd103eb3ecd6ec9450b68544e4c
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-08-07 16:39:09 -0400

    add comments

--- vulnerabilities/mariadb(race)/normal-mariadb/provision/repair-table.sh
+++ vulnerabilities/mariadb(race)/normal-mariadb/provision/repair-table.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# log into mysql as vagrant user (not root) and repair the links table by issuing mysql query
 mysql -uvagrant -pvagrant<<MYSQL_SCRIPT
 REPAIR TABLE normal.links EXTENDED;
 MYSQL_SCRIPT
