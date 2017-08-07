e5b20dbdade7578a626744e62df245f92270f808
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-08-07 16:11:04 -0400

    add comments

--- vulnerabilities/mariadb(race)/normal-mariadb/provision/install_LAMP.sh
+++ vulnerabilities/mariadb(race)/normal-mariadb/provision/install_LAMP.sh
@@ -1,22 +1,20 @@
 cd /home/vagrant
+# download lynx browser for testing
 sudo dnf install -y lynx #for dubugging purposes only 
 
-#install apache
-sudo dnf install -y httpd 
+# install apache http server
+sudo dnf install -y httpd
+# start http damon
 sudo systemctl start httpd.service
+# make sure the next time it will start automatically after reboot
 sudo systemctl enable httpd.service 
 
-#install MySQL (MariaDB)
-# sudo dnf install -y mariadb-server mariadb 
-# sudo systemctl start mariadb 
-# printf '\ny\nvagrant\nvagrant\ny\ny\ny\ny' | mysql_secure_installation  #questions asked 
-# sudo systemctl enable mariadb.service
-
-#install php 
+# install php 
 sudo dnf install -y php php-pdo_mysql
+# restart http to have php
 sudo systemctl restart httpd.service 
 
-#setting up firewall to allow http and https traffic 
+# setting up firewall to allow http and https traffic 
 sudo firewall-cmd --permanent --zone=public --add-service=http
 sudo firewall-cmd --permanent --zone=public --add-service=https
 sudo firewall-cmd --reload
