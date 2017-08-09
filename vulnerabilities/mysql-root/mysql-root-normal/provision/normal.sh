f1d3b75154ef83ecc3e29964e2ab881ea0c9c502
DongyuLi <dennisli1@outlook.com>
2017-08-09 14:24:34 -0400

    Update normal.sh

--- vulnerabilities/mysql-root/mysql-root-normal/provision/normal.sh
+++ vulnerabilities/mysql-root/mysql-root-normal/provision/normal.sh
@@ -6,8 +6,17 @@ sudo mount -o remount,suid /tmp
 
 #config camflow 
 sudo camflow --track-file /usr/bin/mysqld_safe propagate
- 
+
+#starting mysql
 sudo systemctl start mysql
 
+#wait for mysql to get fully started
+sleep 10
+
+# stop camflow capturing 
+sleep 5
+sudo camflow -e false
+# wait a while so that relay fs writes all the data to audit.log
+sleep 30
 #export log
-cp /tmp/audit.log /vagrant/prov-data/audit.log
+cp /tmp/audit.log /vagrant/prov-data/mysql-root-normal-data.log
