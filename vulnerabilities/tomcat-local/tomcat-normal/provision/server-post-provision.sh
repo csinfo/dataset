a802a4c9282e78d81bb8da19326a18dc7a44f6d8
DongyuLi <dennisli1@outlook.com>
2017-08-09 14:46:39 -0400

    Update server-post-provision.sh

--- vulnerabilities/tomcat-local/tomcat-normal/provision/server-post-provision.sh
+++ vulnerabilities/tomcat-local/tomcat-normal/provision/server-post-provision.sh
@@ -1,6 +1,12 @@
 # sudo nmcli connection reload
 # sudo systemctl restart network.service
 
+# stop camflow capturing 
+sleep 5
+sudo camflow -e false
+# wait a while so that relay fs writes all the data to audit.log
+sleep 30
+
 # export provenance data
 sudo cp /tmp/audit.log /vagrant/prov-data/
 #cp /tmp/camflow.clg /vagrant/prov-data/
