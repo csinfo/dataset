6182421c08372cdcb89d23633fc234f9309cdd9a
DongyuLi <dennisli1@outlook.com>
2017-08-09 14:21:22 -0400

    Update normal.sh

--- vulnerabilities/mariadb-root/mariadb-root-normal/provision/normal.sh
+++ vulnerabilities/mariadb-root/mariadb-root-normal/provision/normal.sh
@@ -13,7 +13,14 @@ sudo camflow --track-file /usr/bin/mysqld_safe propagate
 # simulate normal behavior 
 sudo -u root /usr/bin/mysqld_safe start &
 
-sleep 45
+# wait for mysql to get fully started
+sleep 10
+
+# stop camflow capturing 
+sleep 5
+sudo camflow -e false
+# wait a while so that relay fs writes all the data to audit.log
+sleep 30
 # #export log
-cp /tmp/audit.log /vagrant/prov-data/audit.log
+cp /tmp/audit.log /vagrant/prov-data/mariadb-root-normal-data.log
 
