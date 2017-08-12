d1ed6f0fc90055b6b3562e98d3714637938a2e7b
Dongyu Li <dongyul@andrew.cmu.edu>
2017-08-12 16:40:02 -0400

    mysql-race dependency issues resolved for fedora26

--- vulnerabilities/mysql-race/mysql-race-normal/provision/install_mysql.sh
+++ vulnerabilities/mysql-race/mysql-race-normal/provision/install_mysql.sh
@@ -7,7 +7,28 @@ tar xvf MySQL-*.rpm-bundle.tar
 
 # resolve dependency issues 
 #sudo dnf install -y libnuma.so.1 mecab mecab-ipadic perl-Memoize perl-Time-Hires
-sudo dnf install -y libncurses.so.5 libtinfo.so.5 
+
+# sudo dnf install -y libncurses.so.5 libtinfo.so.5 
+
+# wget ftp://rpmfind.net/linux/fedora/linux/releases/23/Everything/x86_64/os/Packages/n/ncurses-base-5.9-21.20150214.fc23.noarch.rpm
+# sudo dnf install -y ncurses-base-5.9-21.20150214.fc23.noarch.rpm
+
+# wget ftp://rpmfind.net/linux/fedora/linux/releases/23/Everything/x86_64/os/Packages/n/ncurses-libs-5.9-21.20150214.fc23.i686.rpm
+# sudo dnf install -y ncurses-libs-5.9-21.20150214.fc23.i686.rpm
+
+# wget ftp://rpmfind.net/linux/fedora/linux/releases/24/Everything/x86_64/os/Packages/n/ncurses-compat-libs-6.0-5.20160116.fc24.i686.rpm
+
+# wget http://invisible-mirror.net/archives/ncurses/current/ncurses-6.0-20170527.tgz
+# tar zxvf ncurses-6.0-20170527.tgz
+# cd ncurses-6.0-20170527
+# sudo ./configure --prefix=/usr --with-shared --with-normal --with-debug --enable-overwrite
+# sudo make 
+# # test nurses in the test dir 
+# sudo make install 
+
+# wget http://dl.fedoraproject.org/pub/fedora/linux/releases/24/Everything/x86_64/os/Packages/n/ncurses-compat-libs-6.0-5.20160116.fc24.x86_64.rpm
+wget ftp://rpmfind.net/linux/fedora/linux/releases/26/Everything/x86_64/os/Packages/n/ncurses-compat-libs-6.0-8.20170212.fc26.x86_64.rpm
+sudo dnf install -y ncurses-compat-libs-6.0-8.20170212.fc26.x86_64.rpm
 
 # install mysql
 sudo rpm -ivh MySQL-*.rpm
