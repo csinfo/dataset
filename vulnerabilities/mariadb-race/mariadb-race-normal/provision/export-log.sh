2869b3567f7ca0c2de70ed51928d72f3e8f5ade2
Michael Han <hanx@g.harvard.edu>
2017-08-29 10:08:15 -0400

    remove extra data

--- vulnerabilities/mariadb-race/mariadb-race-normal/provision/export-log.sh
+++ vulnerabilities/mariadb-race/mariadb-race-normal/provision/export-log.sh
@@ -1,7 +1,7 @@
-# stop camflow capturing 
+# stop camflow capturing
 sleep 5
 sudo camflow -e false
 # wait a while so that relay fs writes all the data to audit.log
 sleep 30
 # copy the audit log to another folder
-cp /tmp/audit.log /vagrant/prov-data/mariadb-race-normal-data.log
+# cp /tmp/audit.log /vagrant/prov-data/mariadb-race-normal-data.log
