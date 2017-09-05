1c290b17c65bca5e5d91228a8e91bcdbc4d0a06e
Michael Han <hanx@g.harvard.edu>
2017-09-05 11:48:56 -0400

    mysql root normal makefile and other files

--- vulnerabilities/mysql-root/mysql-root-normal/provision/normal.sh
+++ vulnerabilities/mysql-root/mysql-root-normal/provision/normal.sh
@@ -1,10 +1,10 @@
 # set selinux to be permissive
-sudo setenforce 0 
+sudo setenforce 0
 
 #remount tmp
-sudo mount -o remount,suid /tmp  
+sudo mount -o remount,suid /tmp
 
-#config camflow 
+#config camflow
 sudo camflow --track-file /usr/bin/mysqld_safe propagate
 
 #starting mysql
@@ -13,10 +13,10 @@ sudo systemctl start mysql
 #wait for mysql to get fully started
 sleep 10
 
-# stop camflow capturing 
+# stop camflow capturing
 sleep 5
 sudo camflow -e false
 # wait a while so that relay fs writes all the data to audit.log
 sleep 30
 #export log
-cp /tmp/audit.log /vagrant/prov-data/mysql-root-normal-data.log
+# cp /tmp/audit.log /vagrant/prov-data/mysql-root-normal-data.log
